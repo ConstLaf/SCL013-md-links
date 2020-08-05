3dbe889ccc8d5b1b0b15423d94ceeabfeb4fceaf
nathaliachn <nathaliachn@gmail.com>
2020-08-05 14:21:14 -0400

    Se añandio chalk al index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const mdlinks = require('./md-links')
 const { dirOrFile } = require('./md-links')
 const { argv } = require('yargs')
+const chalk = require('chalk')
+
+console.log(chalk.blue.bgWhite.bold('----------------- BIENVENID@S A MD-LINKS ---------------------------')) 
 
 if(argv.validate || argv.v) {
     dirOrFile()
